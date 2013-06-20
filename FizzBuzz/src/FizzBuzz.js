4e41af6cd83f82237a800b8499c717458475141c
Eric Fleming <eric-fleming18@hotmail.com>
2013-06-20 18:21:38 -0400

    Green - Input divisible by three now returns Fizz

--- FizzBuzz/src/FizzBuzz.js
+++ FizzBuzz/src/FizzBuzz.js
@@ -1,7 +1,7 @@
 var FizzBuzz = function() {
 	
     var getSentence = function (input) {
-        if (input === 3) {
+        if (input % 3 === 0) {
             return "Fizz";
         }
         else
