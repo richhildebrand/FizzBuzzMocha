8170c3857253180fde2a23a1a4a1201f104b9672
Rich <richhildebrand@gmail.com>
2013-06-20 19:25:17 -0400

    Green - Buzz now return when input divisible by 5

---
+++ FizzBuzz/spec/AcceptanceTests/BuzzFeature.js
@@ -0,0 +1,10 @@
+"use strict";
+
+describe("Acceptance Tests - Buzz Feature", function() {
+
+	describe("Given a number divisible by five FizzBuzz", function() {
+		it("should return Buzz", function() {
+			var numbersNotDivisibleByFive
+		});
+	});
+});
--- FizzBuzz/src/FizzBuzz.js
+++ FizzBuzz/src/FizzBuzz.js
@@ -1,13 +1,17 @@
+"use strict";
+
 var FizzBuzz = function() {
 	
     var getSentence = function (input) {
+    	var sentence = "";
         if (input % 3 === 0) {
-            return "Fizz";
+            sentence = "Fizz";
         }
-        else
+        if  (input % 5 === 0)
         {
-            return "Not";
+			sentence += "Buzz";
         }
+        return sentence;
 	}
 
 	return {
