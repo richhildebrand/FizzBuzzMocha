ca0d75979ef3be98d87f710ea78a4942c720feab
Eric Fleming <eric-fleming18@hotmail.com>
2013-06-20 18:31:31 -0400

    Refactor - Added a semicolon and "test case"

--- FizzBuzz/spec/AcceptanceTests/FizzFeature.js
+++ FizzBuzz/spec/AcceptanceTests/FizzFeature.js
@@ -5,7 +5,7 @@ describe("Acceptance Tests - Fizz Feature", function() {
 	describe("Given a number divisible by three getSentence", function() {
 		it("should return Fizz", function() {
 			var fizzBuzz = new FizzBuzz();
-			var numbersDivisibleByThree = [3, 6, 9, 12, 15, 30, 102, 300]
+			var numbersDivisibleByThree = [3, 6, 9, 12, 15, 30, 102, 300];
 
 			numbersDivisibleByThree.forEach(function(input) {
 				var returnedSentence = fizzBuzz.getSentence(input);
@@ -28,8 +28,12 @@ describe("Acceptance Tests - Fizz Feature", function() {
 	describe("Given a number not divisible by three getSentence", function() {
 		it("should not return Fizz", function() {
 			var fizzBuzz = new FizzBuzz();
-			var returnedSentence = fizzBuzz.getSentence(4);
-			chai.expect(returnedSentence).to.not.contain("Fizz");
+			var numbersNotDivisibleByThree = [4, 5, 7, 8, 10, 20, 100];
+
+			numbersNotDivisibleByThree.forEach(function(input) {
+				var returnedSentence = fizzBuzz.getSentence(input);
+				chai.expect(returnedSentence).to.not.contain("Fizz");
+			});
 		});
 	});
 
