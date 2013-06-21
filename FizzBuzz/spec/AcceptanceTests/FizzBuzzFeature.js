562e6e3e28ad99027fa5e1a9033cc453421e6a10
Eric Fleming <eric-fleming18@hotmail.com>
2013-06-20 22:13:25 -0400

    Red - FizzBuzz feature doesn't work! Fix it!

---
+++ FizzBuzz/spec/AcceptanceTests/FizzBuzzFeature.js
@@ -0,0 +1,16 @@
+"use strict";
+
+describe("Acceptance Tests - FizzBuzz Feature", function() {
+
+	describe("Given a number divisible by three and five getSentence", function() {
+		it("should return FizzBuzz", function() {
+			var fizzBuzz = new FizzBuzz();
+			var numbersDivisibleByThreeAndFive = [15, 30, 45, 300, 900];
+
+			numbersDivisibleByThreeAndFive.forEach(function(input) {
+				var returnedSentence = fizzBuzz.getSentence(input);
+				chai.expect(returnedSentence).to.contain("FizzBuzz");
+			});
+		});
+	});
+});
