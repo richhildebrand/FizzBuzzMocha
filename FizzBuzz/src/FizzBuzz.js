6073d222fa17099b95fb0d96fa2f009a012853ea
Eric Fleming <eric-fleming18@hotmail.com>
2013-06-20 22:00:51 -0400

    Green - Buzz feature now works for numbers divisible and not divisible by 5

--- FizzBuzz/src/FizzBuzz.js
+++ FizzBuzz/src/FizzBuzz.js
@@ -3,9 +3,12 @@
 var FizzBuzz = function() {
 	
     var getSentence = function (input) {
-    	var sentence = "Buzz";
+    	var sentence = "";
         if (input % 3 === 0) {
             sentence += "Fizz";
+        }
+        else if (input % 5 === 0) {
+            sentence += "Buzz";
         }
         return sentence;
 	}
